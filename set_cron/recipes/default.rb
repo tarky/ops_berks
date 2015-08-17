ad69a2f12794f3d92041236bcaae92c778669046
tarky <taisuke.araki@gmail.com>
2015-08-17 13:47:09 +0900

    remove current_release, because there isnot such a variable. remove hard coding of app name.

--- set_cron/recipes/default.rb
+++ set_cron/recipes/default.rb
@@ -1,10 +1,9 @@
-app_name = :inq
+app_name = node[:opsworks][:applications][0][:name]
 rails_env = node[:deploy][app_name][:rails_env]
 env_hash = node[:deploy][app_name][:environment]
-current_release = release_path
 
 execute 'whenever' do
-  cwd current_release
+  cwd "/srv/www/#{app_name}/current"
   command "bundle exec whenever --update-cron -f config/schedule_check.rb"
   environment env_hash.merge(RAILS_ENV: rails_env)
   user 'deploy'
